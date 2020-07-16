255fbac0eba5e449ce712fc3612cf290335104f5
alexgray103 <61517780+alexgray103@users.noreply.github.com>
2020-07-16 14:58:03 -0400

    almost all functions added to ESS_functions

--- Modules/ESS_functions.py
+++ Modules/ESS_functions.py
@@ -1,12 +1,18 @@
 from tkinter import *
-import matplotlib.pyplot as plt
-import ESS_GUI_module
+import matplotlib.pyplot
+
+# modules
+from ESS_GUI_module import *
 import serial
 from settings import Settings as _Settings
+from keyboard import *
+from add_remove_popup import *
+
 import pandas as pd
 import numpy as np
 import csv
-from keyboard import *
+from tkinter import messagebox
+
 from tkinter.filedialog import askopenfilename
 import os
 
@@ -24,15 +30,25 @@ path = '/home/pi/Desktop/Spectrometer/'
 
 
 class functions:
-    def __init__(self, parent, _canvas, figure, df):
+    def __init__(self, parent, _canvas, figure):
         global settings_file
         global acquire_file
+        global ESS_module
         self.parent = parent # whatever parent 
         self.save_file = None # initalize file for saving data
-        self.scan_number = 0 # ID for saving to csv
-        self.reference_number = 0 # ref ID for saving to csv
-    
+        self.scan_number = 1 # ID for saving to csv
+        self.reference_number = 1 # ref ID for saving to csv
+        self.exp_folder = '/home/pi/Desktop/Spectrometer' # experiment folder used for saving
+        self.df = None # data frame array used for storing and plotting data
         
+        # attributes to select data to be plotted
+        self.ref = np.ones((288,1))*1000 # temporary reference
+        
+        # plotting view attributes
+        self.ratio_view_handler = False
+        self.autoscale_handler = False
+        
+            
         # these are two possible port names for the arduino attachment
         port = "/dev/ttyUSB0"
         port2 = "/dev/ttyUSB1"
@@ -40,6 +56,7 @@ class functions:
             self.ser = serial.Serial(port, baudrate = 115200, timeout = 3)
         except:
             self.ser = serial.Serial(port2, baudrate = 115200, timeout =3)
+        
         # two pseudo reads to initalize the spectrometer
         self.ser.write(b'read\n')
         data = self.ser.readline()
@@ -49,18 +66,101 @@ class functions:
         # intialize attributes for handling data and files
         self.acquire_file = acquire_file
         self.settings_file = settings_file
-        self.df = df
         self.canvas = _canvas
         self.fig = figure
+        
+        # create objects for different modules needs for some functions
         self.settings_func = _Settings(settings_file)
         (self.settings, self.wavelength) = self.settings_func.settings_read()
+        self.add_remove_top = add_remove_popup(self.parent)
+   
+    def save_reference(self):
+        self.ref = pd.DataFrame(np.loadtxt(self.acquire_file, delimiter = ','))
+        self.df['Reference %d' % self.reference_number] = self.ref
+        self.df.to_csv(self.save_file, mode = 'w', index = False)
     
+    def save_spectra(self):
+        keyboard = key_pad(self.parent)
+        try:
+            (save_file, save_folder) = keyboard.create_keypad()
+            self.save_file = self.exp_folder + '/' + save_file + "_spectra.csv"
+            
+            open(self.save_file, 'w+')
+            
+            #create data frame for saving data to csv files
+            spectra = pd.DataFrame(self.wavelength)
+            spectra.columns = ['Wavelength (nm)']
+            spectra['Saved Spectra'] = np.loadtxt(self.acquire_file, delimiter = ',')
+            spectra.to_csv(self.save_file, mode = 'a', index=False)
+            # reset scan and ref number for saving data when new file created
+        
+        except NameError:
+           messagebox.showerror("Error", "No Filename Found! Please input again to create Experiment")
+        
+        
+    def add_remove_func(self):
+        self.add_remove_top.create_add_remove(self.save_file)
+        
+        if self.add_remove_top.ref_ratio is not None:
+            self.ref = self.df[[self.add_remove_top.ref_ratio]].to_numpy() 
+        
+    def ratio_view(self):
+        self.ratio_view_handler = not self.ratio_view_handler
+    
+    def autoscale(self):
+        self.autoscale_handler = not self.autoscale_handler
+        if self.autoscale_handler:
+            plt.autoscale(enable= True, axis = 'y')
+            self.fig.canvas.draw()
+        elif not self.autoscale_handler and self.ratio_view_handler:
+            plt.autoscale(enable = False, axis = 'y')
+            plt.ylim(0,110)
+            self.fig.canvas.draw()
+        else:
+            plt.autoscale(enable = False, axis = 'y')
+            plt.ylim(0,66500)
+            self.fig.canvas.draw()
+    
+    def plot_labels_axis(self):
+        if self.ratio_view_handler:
+            plt.plot(self.wavelength, np.ones((288,1))*100, 'r--')
+            if not self.autoscale_handler:
+                plt.ylim(0,110)
+            plt.xlim(300,900)
+            plt.xlabel('Wavelength (nm)')
+            plt.ylabel('Ratio (%)')
+        elif not self.ratio_view_handler:
+            if not self.autoscale_handler:
+                plt.ylim(0,66500)
+            plt.xlim(300,900)
+            plt.xlabel('Wavelength (nm)')
+            plt.ylabel('ADC counts')
+        self.fig.canvas.draw()
+        
+    def plotting(self, data):
+        (self.settings, self.wavelength) = self.settings_func.settings_read()
+        self.plot_labels_axis() # configure axis
+        
+        if self.ratio_view_handler:
+            data = np.true_divide(data, self.ref)*100
+            if self.add_remove_top.data_headers is not None:
+                data_sel = pd.read(self.save_file, header = 0)
+                data_sel = data_sel[self.add_remove_top.data_headers]
+                data_sel = np.true_divide(data_sel,self.ref)*100
+                plt.plot(self.wavelength, data_sel)
+                plt.legend(self.add_remove_top.data_headers, loc = "upper right", prop = {'size': 6})
+        else:
+            pass
+        plt.plot(self.wavelength, data)
+        plt.xlim(int(self.settings[9][1]), int(self.settings[10][1]))
+            
+        
     def open_new_experiment(self):
         global path
         keyboard = key_pad(self.parent)
         try:
             (save_file, save_folder) = keyboard.create_keypad()
-            self.save_file = save_folder + '/' + save_file
+            self.save_file = save_folder + '/' + save_file + "_save.csv"
         
             self.exp_folder = str(save_folder)
             if not os.path.exists(self.exp_folder):
@@ -75,14 +175,29 @@ class functions:
             # reset scan and ref number for saving data when new file created
             self.scan_number = 1
             self.reference_number = 1
+            
         except NameError:
-            pass
+           messagebox.showerror("Error", "No Filename Found! Please input again to create Experiment")
                        
     def plot_selected(self):
         plt.clf()
-        x = range(0,288)
-        plt.plot(x,self.wavelength)
+        try:
+            plt.plot(self.wavelength, self.df[self.add_remove_top.data_headers])
+            plt.legend(self.add_remove_top.data_headers, loc = "upper right", prop ={'size': 6}, bbox_to_anchor = (1.18,0.5))
+            plt.subplots_adjust(bottom =0.14, right = 0.86)
+            self.fig.canvas.draw()
+        except TypeError:
+            messagebox.showerror('Error','No data selected. Select data to plot')
+    
+    def clear(self):
+        plt.clf()
+        plt.ylim(0,66500)
+        plt.xlim(300,900)
+        plt.xlabel('Wavelength (nm)')
+        plt.ylabel('ADC Counts')
         self.fig.canvas.draw()
+        
+        
         
     def dark_subtract_func(self):
         (self.settings, self.wavelength) = self.settings_func.settings_read()
@@ -114,15 +229,14 @@ class functions:
                                        *dummy[i:i+smoothing_width])/(smoothing_width)
         return data_dark
     
-    def acquire_avg(self):
+    def acquire_avg(self, pulses):
         (self.settings, self.wavelength) = self.settings_func.settings_read()
         number_avg = int(self.settings[11][1])
         integ_time =float(self.settings[3][1])
         smoothing_used = int(self.settings[12][1])
         smoothing_width = int(self.settings[8][1])
-        pulses = int(self.settings[1][1])
         dark_subtract = int(self.settings[4][1])
-            
+        
         if dark_subtract == 1:
             data_dark = self.dark_subtract_func()
         else: 
@@ -146,49 +260,128 @@ class functions:
                     for i in range(0,len(data)-smoothing_width,1):
                         data[i] = sum(np.ones(smoothing_width)
                                         *dummy[i:i+smoothing_width])/(smoothing_width)
-        data = data-data_dark        
+        data = data-data_dark
+        for idx in range(0,len(data)):
+            if data[idx] <0:
+                data[idx] = 1
         return data
     
     def acquire(self, save):
-        data = self.acquire_avg()
-        # if acquire and save, save to save_file csv 
+        (self.settings, self.wavelength) = self.settings_func.settings_read()
+        data = self.acquire_avg(int(self.settings[1][1]))
         if save:
-            df_data_array = pd.DataFrame(data)
-            self.df['Scan_ID %d' %self.scan_number] = df_data_array
-            self.df.to_csv(self.save_file, mode = 'w', index = False)
-            data = self.df[['Scan_ID %d' %self.scan_number]]
-            self.scan_number = 1 + self.scan_number
-            self.ratio_view_button.config(state = NORMAL)
-        else: # if only acquire save to temporary file
+            if self.save_file == None:
+                messagebox.showerror('Error', 'No Experiment File Found, create or open File to save spectra')
+            else:
+                # save data array to save_file
+                df_data_array = pd.DataFrame(data)
+                self.df['Scan_ID %d' %self.scan_number] = df_data_array
+                self.df.to_csv(self.save_file, mode = 'w', index = False)
+                data = self.df[['Scan_ID %d' %self.scan_number]]
+                self.scan_number = 1 + self.scan_number
+                self.ratio_view_button.config(state = NORMAL)
+        else: # temporary save
             np.savetxt(self.acquire_file, data, fmt="%d", delimiter=",")
             data = pd.read_csv(self.acquire_file, header = None)
-            # get selected data arrays for plotting
-        plt.clf()
-        plt.plot(self.wavelength, data)
-        self.fig.canvas.draw()
-        '''
-        # if ratio is activated take the ratio of all selected or just current ratio and plot
-        if self.data_headers is not None:
-            data_sel = pd.read_csv(self.save_file, header = 0) # selected data
-            data_sel = data_sel[self.data_headers]
-            if self.autoscale_button['relief'] != SUNKEN: # if autoscale is active, pass, if not set limits to 105%
-                plt.ylim((0,66500))
-            if self.ratio_view_button['relief'] == SUNKEN:
-                data_sel = np.true_divide(data_sel,self.ref)*100 # ratio view activated, overwrite selected data array into ratio
-                data = np.true_divide(data,self.ref)*100
-                plt.plot(self.wavelength, np.ones(288)*100, 'r')
-                if self.autoscale_button['relief'] != SUNKEN: # if autoscale is active, pass, if not set limits to 105%
-                    plt.ylim((0,105))
-            plt.plot(self.wavelength,data_sel[self.data_headers])
-            plt.legend(self.data_headers, loc = "upper right", prop={'size': 6})
-        '''
-    def open_loop(self):
-        x = 1
-    def open_loop_state():
-        if self.open_loop_stop is not None:
-            self.open_loop_button.config(command = open_loop, relief = RAISED, bg = 'light grey')
-            root.after_cancel(self.open_loop_stop)
             
+        plt.clf()
+        self.plotting(data)
+        self.fig.canvas.draw()
+    
+    def open_loop_function(self):
+        (self.settings, self.wavelength) = self.settings_func.settings_read()
+        plt.xlim(int(self.settings[9][1]), int(self.settings[10][1]))
+        plt.ylabel('ADC counts')
+        plt.xlabel('Wavelength (nm)')
+        
+        plt.clf()
+        data = self.acquire_avg(0)
+        np.savetxt(self.acquire_file, data, fmt="%d", delimiter= ",")
+        plt.plot(self.wavelength, data)
+        plt.subplots_adjust(bottom = 0.14, right = 0.86)
+        self.fig.canvas.draw()
+        
+    def sequence(self, save):
+        (self.settings, self.wavelength) = self.settings_func.settings_read()       
+        if save:
+            #try:
+            keyboard = key_pad(self.parent)
+            (seq_file, save_folder) = keyboard.create_keypad()
+            self.seq_file = self.exp_folder + '/' + seq_file + '_sequence.csv'
+            open(self.seq_file, 'x')
+                
+            self.df_seq = pd.DataFrame(self.wavelength)
+            self.df_seq.columns = ['Wavelength (nm)']
+            self.df_seq.to_csv(self.seq_file, mode = "a", index = False)
+            #except:
+            #messagebox.showerror("Error", "No Filename found! Please input file name to save Sequence")
+        else:
+            pass
+        number_avg = int(self.settings[11][1])
+        integ_time = int(self.settings[3][1])
+        smoothing_used = int(self.settings[12][1])
+        smoothing_width = int(self.settings[8][1])
+        dark_subtract = int(self.settings[4][1])
+        burst_number = int(self.settings[22][1])
+        burst_delay = float(self.settings[21][1])
+        
+        plt.xlim(int(self.settings[9][1]), int(self.settings[10][1]))
+        
+        for burst in range(0,burst_number):
+            number_measurements_burst = int(self.settings[23+burst][1])
+            measurement = 0
+            for i in range(0,number_measurements_burst):
+                graph_label = 'Burst ' + str(burst+1) + ' measurement ' + str(i+1)
+                pulses = int(self.settings[33+burst][1])
+                data = self.acquire_avg(pulses)
+    
+                plt.plot(self.wavelength, data, label = graph_label)
+                plt.subplots_adjust(bottom = 0.14, right = 0.80)
+                plt.legend(loc = "center right", prop = {'size': 6}, bbox_to_anchor = (1.285,0.5))
+                self.fig.canvas.draw()
+                measurement = measurement+1
+                
+                if save:
+                    df_data_array = pd.DataFrame(data)
+                    self.df_seq['Burst %d Measurement %d' % (burst+1, measurement)] = df_data_array
+            sleep(burst_delay)
+        # after all data is taken save to sequence csv
+        if save:
+            self.df_seq.to_csv(self.seq_file, mode = 'w', index = False)
+    
+    def autorange(self):
+        (self.settings, self.wavelength) = self.settings_func.settings_read()       
+        max_autorange = int(self.settings[7][1])
+        autorange_thresh = int(self.settings[6][1])
+        integ_time = int(self.settings[3][1])
+        pulses = 1 # start with one pulse then increment
+        plt.clf()
+        
+        plt.xlim(int(self.settings[9][1]), int(self.settings[10][1])) # change x axis limits to specified settings
+        plt.ylabel('a.u.')
+        plt.xlabel('Wavelength (nm)')
+        # acquire data for the given # of loops plot, and prompt user to
+        # select plots they wish to save with a popup window
+        for x in range(0,max_autorange): 
+            self.settings[1][1] = pulses
+            # write settings array to csv 
+            self.settings_func.settings_write(self.settings)
+            data = self.acquire_avg(pulses)
+            if max(data) < autorange_thresh:  
+                if x < max_autorange-1:
+                    pulses = pulses+1
+                    plt.plot(self.wavelength,data, label = "Pulses: "+ str(self.settings[1][1]))
+                    plt.subplots_adjust(bottom=0.14, right=0.86)
+                    plt.legend(loc = "center right", prop={'size': 7}, bbox_to_anchor=(1.18, 0.5))
+                    self.fig.canvas.draw()
+                else: 
+                    messagebox.showinfo("Pulses", "Max # of Pulses reached")
+            else:
+                self.settings[1][1] = pulses-1
+                messagebox.showinfo("Pulses", str(self.settings[1][1]) + "  Pulses to reach threshold")
+                break
+        self.settings_func.settings_write(self.settings)
+        
     def OpenFile(self):
             
         save_file = askopenfilename(initialdir="/home/pi/Desktop/Spectrometer",
@@ -197,7 +390,9 @@ class functions:
         #try:
         if save_file: # check if file was selected if not dont change experiment file
             self.save_file = save_file 
-                
+            self.reference_number = 1
+            self.scan_number = 1
+            
             # try to scan through reference and scan number to set to correct value for further saving
             self.df = pd.read_csv(self.save_file, header = 0)
             headers = list(self.df.columns.values)
@@ -215,14 +410,12 @@ class functions:
                         break
                 self.reference_number = self.reference_number+1
                 self.ref = pd.DataFrame(self.df['Reference %d' %(self.reference_number-1)])
-            
+
             #reset indexing attrubtues for later use in
             #add remove functions 
-            self.data_headers_idx = None
-            self.data_headers = None
-            self.reference_ratio = None
-            self.reference_ratio_idx = None
-            self.parent.open_loop_button.config(state = NORMAL)
-        #except:
-        #messagebox.showerror("Error", "Oops! Something went wrong. Try Again!")
-    
+            self.add_remove_top.data_headers_idx = None
+            self.add_remove_top.data_headers = None
+            self.add_remove_top.ref_ratio = None
+            self.add_remove_top.ref_ratio_idx = None
+            
+        
